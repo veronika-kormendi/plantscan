c4a13cb28ce60459cf1c7c07671093612feb15a2
Veronika <veronika.kormendi@hotmail.com>
2026-02-08 22:12:14 +0000

    add HEIC to JPG conversion

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,18 @@
+from PIL import Image
+import pillow_heif
+
+# register HEIF opener with Pillow
+pillow_heif.register_heif_opener()
+
+# open the HEIC file
+img = Image.open('G:\\My Drive\\plantscan_photos_to_process\\IMG_7522.HEIC')
+
+# save image as JPG
+img.save('G:\\My Drive\\plantscan_photos_to_process\\IMG_7522.jpg', format='JPEG')
+
+
 def hello_world():
     print("Hello World!")
 
 hello_world()
+
