28a1d8dedd33fa97326167383c31393ff3ce970b
Veronika <veronika.kormendi@hotmail.com>
2026-02-23 00:35:42 +0000

    update main.py

--- main.py
+++ main.py
@@ -1,59 +1,60 @@
 ''' Author: Veronika Kormendi
     Purpose: Final Year Project
 '''
+
+# ------- IMPORTS -------
 import os
+from PIL import Image, ImageTk  # for managing images
+import pillow_heif # for HEIC to JPG conversion
+import tkinter as tk # for GUI
+import cv2 as cv # openCV
+import ttkbootstrap as ttk # for modern GUI
 from tkinter import filedialog
 
-from PIL import Image, ImageTk  # for managing images
-import pillow_heif
-import cv2 as cv
-import tkinter as tk # for GUI
-import ttkbootstrap as ttk # for modern GUI
-import numpy as np
 
 # step 1 - img conversion: HEIC to JPG
 
 input_folder = 'G:\\My Drive\\plantscan_photos_to_process'
 output_folder = 'G:\\My Drive\\plantscan_photos_to_process\\converted_to_jpg'
 
-# """save HEIC to JPG path"""
-# def heic_to_jpg(heic_path, jpg_path):
-#     img = Image.open(heic_path)
-#     img.save(jpg_path, format='JPEG')
-#
-# def convert_multiple_heic_to_jpg(input_folder, output_folder):
-#     # register HEIF opener with Pillow
-#     pillow_heif.register_heif_opener()
-#     # create output folder if it does not exist
-#     if not os.path.exists(output_folder):
-#         os.makedirs(output_folder)
-#     for filename in os.listdir(input_folder):
-#         if filename.lower().endswith('.heic'):
-#             heic_path = os.path.join(input_folder, filename)
-#             jpg_filename = f"{os.path.splitext(filename)[0]}.jpg"
-#             jpg_path = os.path.join(output_folder, jpg_filename)
-#             heic_to_jpg(heic_path, jpg_path)
-#             print(f"Converting {filename} to JPG")
-#
-# # step 2 - colour to greyscale img
-#
-# convert_multiple_heic_to_jpg(input_folder, output_folder)
-#
-# greyscale_input_folder = 'G:\\My Drive\\plantscan_photos_to_process\\converted_to_jpg'
-# greyscale_output_folder = 'G:\\My Drive\\plantscan_photos_to_process\\greyscale'
-# def colour_to_greyscale(greyscale_input_folder, greyscale_output_folder):
-#     if not os.path.exists(greyscale_output_folder):
-#         os.makedirs(greyscale_output_folder)
-#     for file in os.listdir(greyscale_input_folder):
-#         colour_path = os.path.join(greyscale_input_folder, file)
-#         greyscale_path = os.path.join(greyscale_output_folder, file)
-#         img = cv.imread(colour_path) # read image
-#         greyscale_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY) # convert to greyscale
-#         cv.imwrite(greyscale_path, greyscale_img) # saving the greyscale img to the output folder
-#         print(f"converting {file} to greyscale")
-# colour_to_greyscale(greyscale_input_folder, greyscale_output_folder)
+# convert all images at once
+def heic_to_jpg(heic_path, jpg_path):
+    img = Image.open(heic_path)
+    img.save(jpg_path, format='JPEG')
 
-# ----------GUI------------
+def convert_multiple_heic_to_jpg(input_folder, output_folder):
+    # register HEIF opener with Pillow
+    pillow_heif.register_heif_opener()
+    # create output folder if it does not exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    for filename in os.listdir(input_folder):
+        if filename.lower().endswith('.heic'):
+            heic_path = os.path.join(input_folder, filename)
+            jpg_filename = f"{os.path.splitext(filename)[0]}.jpg"
+            jpg_path = os.path.join(output_folder, jpg_filename)
+            heic_to_jpg(heic_path, jpg_path)
+            print(f"Converting {filename} to JPG")
+
+convert_multiple_heic_to_jpg(input_folder, output_folder)
+
+# step 2 - colour to greyscale img
+greyscale_input_folder = 'G:\\My Drive\\plantscan_photos_to_process\\converted_to_jpg'
+greyscale_output_folder = 'G:\\My Drive\\plantscan_photos_to_process\\greyscale'
+
+def colour_to_greyscale(greyscale_input_folder, greyscale_output_folder):
+    if not os.path.exists(greyscale_output_folder):
+        os.makedirs(greyscale_output_folder)
+    for file in os.listdir(greyscale_input_folder):
+        colour_path = os.path.join(greyscale_input_folder, file)
+        greyscale_path = os.path.join(greyscale_output_folder, file)
+        img = cv.imread(colour_path) # read image
+        greyscale_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY) # convert to greyscale
+        cv.imwrite(greyscale_path, greyscale_img) # saving the greyscale img to the output folder
+        print(f"converting {file} to greyscale")
+colour_to_greyscale(greyscale_input_folder, greyscale_output_folder)
+
+# ---------- GUI ------------
 gui_window = tk.Tk() # creating a window instance
 gui_window.title("is_it_plant_based?") # add gui window title
 gui_window.iconbitmap('C:\\Users\\veron\\Downloads\\isitpb.ico') # adding icon to gui window
@@ -62,6 +63,7 @@ width = gui_window.winfo_screenwidth() # set window width
 height = gui_window.winfo_screenheight() # set window height
 gui_window.geometry(f"{width}x{height}+0+0") # window size with width, height, offset, offset
 
+# resize image proportionately to fit to screen
 def resize_image(image):
     img_w, img_h = image.size
     new_w = 600
@@ -75,23 +77,21 @@ img = resize_image(img) # resizing the image
 img_tk = ImageTk.PhotoImage(img) # convert to tkinter compatible format
 gui_window.imgtk = img_tk
 
+
+def cropClick():
+    my_label = tk.Label(gui_window, text="Photo crop complete")
+    my_label.pack()
+my_button = tk.Button(gui_window, text="Crop", padx=10, pady=2, command=cropClick) # displays confirmation message when photo is cropped
+my_button.pack() #display button
+cropClick()
+
+
 def display_image(image):
     canvas = tk.Canvas(gui_window, width=width, height=height) # creating a canvas to display the image on
     canvas.create_image(10,10, anchor=tk.NW, image=img_tk) # adding tkinter image to canvas
     canvas.pack() # display canvas with its contents
 
 display_image(img_tk)
-
-
-# def cropClick():
-#     my_label = tk.Label(gui_window, text="Photo crop complete")
-#     my_label.pack()
-# my_button = tk.Button(gui_window, text="Crop", padx=10, pady=2, command=cropClick) # displays confirmation message when photo is cropped
-# my_button.pack() #display button
-# cropClick()
-
-
-
 
 # def doSomething(event):
 #     print("Doing something")
